270fa5124adaea138f917bd22ea35171f934afbd
IvanGMtz <navi.garces24@gmail.com>
2023-05-29 17:32:25 -0500

    feat: :zap: numbers and operators

--- index.php
+++ index.php
@@ -1,33 +1,34 @@
 <?php
     /**
-     * TIPOS DE DATOS
-     *  @param logueado *Variable booleana.
-     * @param numero *Variable de númerica.
-     * @param Float *Variable flotante.
-     * @param nombre *Variable de texto.
-     * @param Array *Variable de tipo .
+     * Números y Operadores 
+     *  @param N1 *Variable númerica.
+     * @param N2yN3 *Variable de númerica de igual valor.
+     * @param N4 *Variable de valor 30 y tipo string.
      */
 
-    $logueado = true;
-    var_dump($logueado);
+    $N1=20;
+    $N2=30;
+    $N3=30;
+    $N4="30";
 
+    var_dump($N1 > $N2); // N1 mayor que N2
     echo("<br>");
 
-    $numero = 200;
-    var_dump($numero);
-
+    var_dump($N1 < $N2); // N1 menor que N2
     echo("<br>");
 
-    $float = 15.2;
-    var_dump($float);
-
+    var_dump($N1 >= $N2); // N1 mayor igual que N2
     echo("<br>");
 
-    $nombre = "Ivan";
-    var_dump($nombre);
-
+    var_dump($N1 <= $N2); // N1 menor igual que N2
     echo("<br>");
 
-    $array = [];
-    var_dump($array);
+    var_dump($N1 <= $N2); // N1 menor igual que N2
+    echo("<br>");
+
+    var_dump($N1 == $N2); // N1 igual valor que N2
+    echo("<br>");
+
+    var_dump($N3 === $N4); // N3 igual evaluando tipo y valor que N4
+    echo("<br>");
 ?>
