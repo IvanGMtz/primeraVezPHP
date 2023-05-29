28d8f06df3a81c747f351e6e4944bce5f6b62382
IvanGMtz <navi.garces24@gmail.com>
2023-05-29 16:51:37 -0500

    feat: :lipstick: Start PHP
    Basic structure and  echo

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    /**
+     * Se implementó la estructura basica de un script PHP con cada una de las carpetas
+     *  @param echo *Función basica para imprimir desde el PHP al html
+     */
+    echo "<h1>Hola mundo cruel</h1>";
+    
+?>
