06ba9dc795fecde1515ce3a34417af8667e6eb37
IvanGMtz <navi.garces24@gmail.com>
2023-06-01 17:03:33 -0500

    feat: :sparkles: Introduce Arrays and methods in PHP

--- index.php
+++ index.php
@@ -1,34 +1,165 @@
 <?php
-    /**
-     * Números y Operadores 
-     *  @param N1 *Variable númerica.
-     * @param N2yN3 *Variable de númerica de igual valor.
-     * @param N4 *Variable de valor 30 y tipo string.
-     */
+**
+* Todo Array indexado*
+* Array que a cada posición se le asigna un número partiendo de 0.
+*/
 
-    $N1=20;
-    $N2=30;
-    $N3=30;
-    $N4="30";
+/**
+* ? nombres Ejemplo de Array indexado*
+*/
+$nombres = array("Juan", "Pedro", "Santiago", "Jesus", "Ivan", "Diego", "Cristian");
 
-    var_dump($N1 > $N2); // N1 mayor que N2
-    echo("<br>");
+var_dump($nombres);
+echo("<hr>");
 
-    var_dump($N1 < $N2); // N1 menor que N2
-    echo("<br>");
+/**
+* Todo Array asociativo*
+* Array llave => valor.
+*/
 
-    var_dump($N1 >= $N2); // N1 mayor igual que N2
-    echo("<br>");
+/**
+* ? nombres Ejemplo de Array asociativo*
+*/
 
-    var_dump($N1 <= $N2); // N1 menor igual que N2
-    echo("<br>");
+$edades = array(
+   "Juan" => 35,
+   "Pedro" => 37,
+   "Santiago" =>43);
 
-    var_dump($N1 <= $N2); // N1 menor igual que N2
-    echo("<br>");
+var_dump($edades);
+echo("<hr>");
 
-    var_dump($N1 == $N2); // N1 igual valor que N2
-    echo("<br>");
+/**
+* Todo Array Multidimensional*
+* Array asociativo con uno o más arrays asociativos por dentro.
+*/
 
-    var_dump($N3 === $N4); // N3 igual evaluando tipo y valor que N4
-    echo("<br>");
+/**
+* ? nombres Ejemplo de Array Multidimensional*
+*/
+
+$personas= array(
+   "Juan" => array("Edad"=>23, "Ciudad"=> "Madrid", "País" => "España"),
+   "Pedro" => array("Edad"=>37, "Ciudad"=> "Paris", "País" => "Francia"),
+   "Santiago" =>array("Edad"=>43, "Ciudad"=> "Bogota", "País" => "Colombia"));
+/**
+* ? Agregar información al array*
+*/
+   $personas["Pedro"]["Altura"] = 1.63;
+   print_r($personas);
+
+/**
+* ? Ejemplo consulta al array*
+*/
+   print_r($personas["Pedro"]["Ciudad"]);
+
+   echo("<hr>");
+   echo("<hr>");
+   echo("<hr>");
+/**
+* ! METODOS MÁS USADOS PARA ARRAYS*
+* array_flip(): Intercambia las claves con sus valores correspondientes en un array.
+* array_fill(): Rellena un array con un valor especificado.
+* array_filter(): Filtra los elementos de un array utilizando una función de devolución de llamada.
+* array_map(): Aplica una función a cada elemento de un array y devuelve un nuevo array con los resultados.
+* array_reduce(): Reduce un array a un solo valor aplicando una función de devolución de llamada.
+* array_key_exists(): Comprueba si una clave existe en un array.
+* in_array(): Comprueba si un valor existe en un array.
+* array_rand(): Devuelve una o varias claves aleatorias de un array.
+array_unique(): Elimina los valores duplicados de un array.
+array_intersect(): Devuelve un array con los valores comunes a todos los arrays dados.
+array_diff(): Devuelve un array con los valores del primer array que no están presentes en los arrays adicionales.
+array_push(): Agrega uno o más elementos al final de un array.
+array_pop(): Extrae y elimina el último elemento de un array.
+array_reverse(): Revierte el orden de los elementos en un array.
+array_sum(): Devuelve la suma de todos los valores de un array numérico.
+array_product(): Devuelve el producto de todos los valores de un array numérico.
+array_chunk(): Divide un array en fragmentos más pequeños.
+array_keys(): Devuelve todas las claves de un array.
+array_values(): Devuelve todos los valores de un array.
+array_walk(): Aplica una función de devolución de llamada a cada elemento de un array.
+*/
+
+/**
+* ? Ejemplo de array_flip(): con array nombres*
+*/
+echo("<h1>array_flip</h1>"); 
+$Flip = array_flip($nombres);
+print_r($Flip);
+
+/**
+* ? Ejemplo de array_fill()*
+*/
+echo("<h1>array_fill</h1>"); 
+$LoteriaA= array_fill(0, 11, "Número");
+$LoteriaB= array_fill(-10, 5, "Número");
+print_r($LoteriaA);
+echo("<br>");echo("<br>");
+print_r($LoteriaB);
+
+/**
+* ? Ejemplo de array_filter() con array nombres*
+*/
+echo("<h1>array_filter</h1>"); 
+
+print_r(array_filter($nombres, function($k) {
+   return $k > 4;
+}, ARRAY_FILTER_USE_KEY));
+echo("<br>");echo("<br>");
+print_r(array_filter($nombres, function($v, $k) {
+   return $k > 4 || $v ==="Ivan";
+}, ARRAY_FILTER_USE_BOTH));
+
+/**
+* ? Ejemplo de array_map() con array nombres*
+*/
+echo("<h1>array_map</h1>");
+
+function saludos($arr){
+   return "Hola me llamo $arr";
+}
+$saludo= array_map("saludos", $nombres);
+print_r($saludo);
+
+/**
+* ? Ejemplo de array_reduce()*
+*/
+echo("<h1>array_reduce</h1>");
+
+$PruebaReduce = array(0,1,2,3,4,5,6,7,8,9,10);
+
+function Sum($carry, $item)
+{
+   $carry += $item;
+   return ($carry);
+}
+$Suma =array_reduce($PruebaReduce, "Sum");
+print_r($Suma);
+echo("<br>");echo("<br>");
+print_r($Suma/count($PruebaReduce)); //Promedio, usando el metodo count()
+
+/**
+* ? Ejemplo de array_key_exists() con array edades*
+*/
+echo("<h1>array_key_exists</h1>");
+if (array_key_exists("Juan", $edades)) {
+   echo "Si se encuentra";
+}else{
+   echo "No se encuentra";
+};
+
+/**
+* ? Ejemplo de in_array() con array edades*
+*/
+echo("<h1>in_array</h1>");
+if(in_array(43, $edades)){
+   echo "Si se encuentra";
+}else{
+   echo "No se encuentra";
+};
+
+/**
+* ? Ejemplo de array_rand() con array edades*
+*/
+echo("<h1>array_rand</h1>");
 ?>
